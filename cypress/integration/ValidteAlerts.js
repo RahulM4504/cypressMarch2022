fc74c6015c81f84a6a47a7e22613d108827d308c
Rahul Mhatre <rahulmhatre4504@gmail.com>
2022-02-28 23:12:40 +0530

    This is my first cypress commit

---
+++ cypress/integration/ValidteAlerts.js
@@ -0,0 +1,26 @@
+describe('Validate Alerts', () => {
+
+    it('Grab alert text in cypress',()=>{
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+
+        cy.get('input#name').type("hello").type('{selectall}')
+        cy.get('input#name').rightclick().type('{downarrow}').type('{downarrow}').type('{enter}')
+
+        //,'{downarrow}','{downarrow}','{enter}')
+
+        // cy.get('#alertbtn').click();
+        // cy.on('window.alert',(alertstr)=>{
+
+        //     expect(alertstr).equal('Hello , share this practice page and share your knowledge')
+        // })
+
+    })
+
+    // it('',()=>{
+
+    // })
+
+    // it('',()=>{
+
+    // })
+});
